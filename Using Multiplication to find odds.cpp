55654b7118c3d8eb1f33be8611ef3ca0dc323a45
Mondotrasho <oscar@peoplepictures.com.au>
2018-02-06 15:10:09 +1100

    Better quicker code

---
+++ Using Multiplication to find odds.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+	int numberOfOdds = 15;
+	int total = numberOfOdds * numberOfOdds;
+
+	std::cout << "total of the first " << numberOfOdds << " odd numbers is " << total << std::endl;
+
+	system("pause");
+
+		return 0;
+}
